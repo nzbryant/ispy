458d229b8e036337f5daf9a59e337738c96bf6e5
Charles F. Munat <charles.munat@gmail.com>
2013-05-22 10:54:15 +0100

    Added user and objective stores.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,5 @@
 App = Ember.Application.create();
 
-App.Router.map(function() {
-  this.resource('objectives');
-  this.resource('users');
-});
-
-App.IndexRoute = Ember.Route.extend({
-  redirect: function() {
-    this.transitionTo('objectives');
-  }
-});
-
-App.Model = Ember.Object.extend({
-  fields: [],
-  
-  forWire: function() {
-    return this.getProperties(this.get('fields'));
-  }
-});
-
 App.Store = Ember.Object.extend({
   init: function() {
     this._super();
@@ -51,5 +32,67 @@ App.Store = Ember.Object.extend({
     var idMap = this.get('idMap');
     
     return idMap[id];
+  }
+});
+
+App.initializer({
+  name: 'stores',
+  initialize: function() {
+    App.User.store = App.UserStore.create();
+    App.Objective.store = App.ObjectiveStore.create();
+  }
+});
+
+App.Router.map(function() {
+  this.resource('objectives');
+  this.resource('users');
+});
+
+App.IndexRoute = Ember.Route.extend({
+  redirect: function() {
+    this.transitionTo('objectives');
+  }
+});
+
+App.Model = Ember.Object.extend({
+  fields: [],
+  
+  forWire: function() {
+    return this.getProperties(this.get('fields'));
+  }
+});
+
+App.User = App.Model.extend({
+  fields: ['id', 'name', 'email']
+});
+
+App.UserStore = App.Store.extend({
+  name: 'users',
+  model: App.User,
+  deserialize: function(properties) {
+    return {
+      id: properties.id,
+      name: properties.name,
+      email: properties.email
+    }
+  }
+});
+
+App.Objective = App.Model.extend({
+  fields: ['id', 'name', 'createdAt', 'description', 'location', 'coordinates', 'address']
+});
+
+App.ObjectiveStore = App.Store.extend({
+  name: 'objectives',
+  model: App.Objective,
+  deserialize: function(properties) {
+    return {
+      name: properties.name,
+      createdAt: Date.parse(properties.createdAt),
+      description: properties.description,
+      location: properties.location,
+      coordinates: properties.coordinates,
+      address: properties.address
+    }
   }
 });
