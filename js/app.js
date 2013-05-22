42afddc9b22082975253685ee1299a9fa848a62d
Charles F. Munat <charles.munat@gmail.com>
2013-05-22 10:34:08 +0100

    Got a start on the store.

--- js/app.js
+++ js/app.js
@@ -10,3 +10,46 @@ App.IndexRoute = Ember.Route.extend({
     this.transitionTo('objectives');
   }
 });
+
+App.Model = Ember.Object.extend({
+  fields: [],
+  
+  forWire: function() {
+    return this.getProperties(this.get('fields'));
+  }
+});
+
+App.Store = Ember.Object.extend({
+  init: function() {
+    this._super();
+    this.set('idMap', {});
+    this.set('hydratedObjects', []);
+    this._createBucket();
+  },
+  
+  all: function() {
+    return this.get('hydratedObjects');
+  },
+  
+  find: function(id) {
+    return this._objectFor(id);
+  },
+  
+  createRecord: function(properties) {
+    var id = moment().valueOf().toString()
+    properties.id = id;
+    var object = this.find(id);
+    object.setProperties(properties);
+    return object;
+  },
+  
+  _createBucket: function() {
+    
+  },
+  
+  _objectFor: function(id) {
+    var idMap = this.get('idMap');
+    
+    return idMap[id];
+  }
+});
